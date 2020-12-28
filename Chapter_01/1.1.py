568886152f70077856d42aeed81461d066cae4ee
Bill Mei <billmei@users.noreply.github.com>
2020-12-28 15:57:38 -0500

    1.1

---
+++ Chapter_01/1.1.py
@@ -0,0 +1,44 @@
+# Determine if a string has all unique characters
+
+# Idea 1: Use a hashmap to track the characters used so far.
+# This is O(n), but requires an additional data structure
+def is_unique(chars):
+    seen = {}
+    for char in chars:
+        if seen.get(char):
+            return False
+
+        seen[char] = 1
+
+    return True
+
+# Idea 2: Make a bit mask for each character, and check there is no overlap
+# This is O(n), does not require an additional data structure, but
+# this isn't memory efficient if you have a large alphabet, e.g. unicode
+import string
+alphabet = list(string.printable)
+def is_unique(chars):
+    vec = [0] * len(alphabet) # length of alphabet
+    for char in chars:
+        idx = alphabet.index(char)
+        if vec[idx]:
+            return False
+        vec[idx] = 1
+    return True
+
+# Idea 3: Sort the string, and then make sure every character is different from the previous one
+# But this takes O(n log n) time, compared to O(n) time.
+def is_unique(chars):
+    last_char = ""
+    for char in sorted(chars):
+        if char == last_char:
+            return False
+
+        last_char = char
+    return True
+
+assert is_unique("abc")
+assert not is_unique("abcb")
+assert is_unique("")
+assert is_unique(" ")
+print("All tests passed")
